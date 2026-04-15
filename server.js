e213ae52483e516b8ca0089192d7dd579d0ce1c3
harsh-shahi <harshkumarshahi421@gmail.com>
2026-04-15 21:54:39 +0530

    second

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/admin.html");
+});
 
 
 // ================= HASH FUNCTION =================
